5146b9b450ddaa191a86eb6dd07433a276c0014b
Latish705 <latishadwani705@gmail.com>
2024-07-07 16:01:16 +0530

    initialzing client and server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import connnectDB from "./config/db";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+connnectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
